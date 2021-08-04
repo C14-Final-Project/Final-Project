9a293c542d099701657a7a5df74d5724882e10d8
adammathewvaughn <adammathewvaughn@gmail.com>
2021-08-04 09:20:26 -0500

    trying to sync with the team

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import './scss/app';
+render(React.createElement(App, null), document.getElementById("root"));
