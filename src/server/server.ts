b1f7828813f491e710370680b9a5cfa7e825ad38
theconmcdon <83612766+theconmcdon@users.noreply.github.com>
2021-08-05 08:31:13 -0500

    Revert "Adam"

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-import * as path from 'path';
 import apiRouter from "./routes";
 
 const app = express();
@@ -8,6 +7,5 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/api', apiRouter);
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 app.listen(3000, () => console.log('App listening on port 3000'));
